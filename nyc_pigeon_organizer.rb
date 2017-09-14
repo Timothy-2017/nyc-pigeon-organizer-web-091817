9b7b69e5ae6dabc85e542629deda01080af14589
Tim Eubank <timothy.eubank@gmail.com>
2017-09-14 22:35:59 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,18 @@
+require "pry"
+
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  pigeon_list = {}
+  data.each do |key_trait, value_specifics|
+    #binding.pry
+    value_specifics.each do |key_specifics, value_names|
+      #binding.pry
+      value_names.each do |name|
+        #binding.pry
+        pigeon_list[name] ||= {}
+        pigeon_list[name][key_trait] ||= []
+        pigeon_list[name][key_trait] << key_specifics.to_s
+      end
+    end
+  end
+  pigeon_list
 end
